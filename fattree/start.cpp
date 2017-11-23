357c2200d893bd1aada646258edde1537ca66707
CarpeDiem <c05267@gmail.com>
2017-11-23 22:21:04 +0800

    fix printf

--- fattree/start.cpp
+++ fattree/start.cpp
@@ -295,7 +295,7 @@ void Fattree::start(void){
 						printf("# of flow setup request: %d\n", metric_flowSetupRequest);
 						printf("# of installed rules: %d\n", metric_ruleInstallCount);
 						printf("Avg. flow completion time: %.3lf\n", metric_avgFlowCompleteTime/totFlow);
-						printf("Avg. flow setup: %d rule install: %d\n rule rep: %d", fs,ri,re);
+						printf("Avg. flow setup: %d rule install: %d rule rep: %d\n", fs,ri,re);
 						printf("Wireless:Wired = %d:%d\n", numberOfWirelessFlow, numberOfWiredFlow);
 						printf("Replacement %d / %d / %d\n", ruleReplacementCore, ruleReplacementAggr, ruleReplacementEdge);
 						/*for(int i=0; i<numberOfCore+numberOfAggregate+numberOfEdge; i++)
